f34b484e6856958a0998c58a28514b6b40aa383f
volodymyr32 <volodymyr@payfurl.com>
2024-12-05 14:35:58 -0700

    PAY-3638 - fixed assert errors

--- src/test/java/com/payfurl/payfurlsdk/apitesting/SubscriptionApiTest.java
+++ src/test/java/com/payfurl/payfurlsdk/apitesting/SubscriptionApiTest.java
@@ -144,8 +144,8 @@ public class SubscriptionApiTest {
         assertThat(subscriptionDataUpdated.getSubscriptionId()).isEqualTo(subscriptionData.getSubscriptionId());
         assertThat(subscriptionDataUpdated.getAmount()).isEqualTo(BigDecimal.valueOf(200));
         assertThat(subscriptionDataUpdated.getCurrency()).isEqualTo("AUD");
-        assertThat(subscriptionDataUpdated.getInterval()).isEqualTo(SubscriptionInterval.Month);
-        assertThat(subscriptionDataUpdated.getFrequency()).isEqualTo(1);
+        assertThat(subscriptionDataUpdated.getInterval()).isEqualTo("Month");
+        assertThat(subscriptionDataUpdated.getFrequency()).isEqualTo(Interger.valueOf(1);
         assertThat(subscriptionDataUpdated.getEndAfter()).isNull();
         assertThat(subscriptionDataUpdated.getRetry()).isNull();
         assertThat(subscriptionDataUpdated.getWebhook()).isNull();
